db91ef58c26ce84d44bc4137a2c4fdfa64042147
Nina Leung <nleung320@gmail.com>
2015-11-24 14:17:18 -0600

    added background and css

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,12 @@
 class ContactsController < ApplicationController
   def index
     @contacts = Contact.all
+
+    search_term = params[:search]
+    if search_term
+      # @contacts = Contact.where("first_name LIKE ? OR last_name LIKE ?", "%#{search_term}, %#{search_term}%" )
+      @contacts = Contact.where("first_name LIKE ? OR last_name LIKE ?", "%#{search_term}%", "%#{search_term}%")
+    end
   end
 
   def new
@@ -13,6 +19,7 @@ class ContactsController < ApplicationController
       phone: params[:phone],
       email: params[:email]
       )
+    flash[:success] = "Contact successfully created!"
     redirect_to '/contacts'
   end
 
@@ -25,10 +32,21 @@ class ContactsController < ApplicationController
   end
 
   def update
-    
+    @contact = Contact.find_by(id: params[:id])
+    @contact.update(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      phone: params[:phone],
+      email: params[:email]
+      )
+    flash[:success] = "Contact successfully updated!"
+    redirect_to "/contacts/#{@contact.id}"
   end
 
   def destroy
-    
+    @contact = Contact.find_by(id: params[:id])
+    @contact.destroy
+    flash[:success] = "Contact successfully deleted!"
+    redirect_to "/contacts/"
   end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,13 @@
 class Contact < ActiveRecord::Base
+  def readable_updated_on
+    updated_at.strftime("%A, %d %b %Y at %l:%M %p")    
+  end
+
+  def full_name
+    first_name + " " + last_name
+  end
+
+  def japanese_prefix
+    "+81" + " " + phone
+  end
 end
