090607bfb671424e90a5cabe546caa8446ad22c7
Nina Leung <nleung320@gmail.com>
2015-11-24 21:28:54 -0600

    accidentally added order model to this app instead of mini-capstone

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151125004355) do
+ActiveRecord::Schema.define(version: 20151125030856) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "first_name"
@@ -21,6 +21,18 @@ ActiveRecord::Schema.define(version: 20151125004355) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "product_id"
+    t.integer  "quantity"
+    t.decimal  "subtotal",   precision: 10, scale: 2
+    t.decimal  "decimal",    precision: 10, scale: 2
+    t.decimal  "tax",        precision: 10, scale: 2
+    t.decimal  "total",      precision: 10, scale: 2
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
   create_table "users", force: :cascade do |t|
